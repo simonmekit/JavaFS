476b0b6066df09d75cfa59c8889ea4a6710da3be
simon mekit <simonmekit@gmail.com>
2023-12-15 16:55:11 -0500

    Revert "Java Basics labs"
    This reverts commit 99a8d305b84336307e91284b5ceeb6b84e51dbb1.

--- src/com/perscholas/java_basics/TernaryOperatorDemo.java
+++
@@ -1,9 +0,0 @@
-package com.perscholas.java_basics;
-
-public class TernaryOperatorDemo {
-    public static void main(String[] args) {
-        int age = 18;
-        String result = age < 100 ? "Less than 100" : "Greater than 100";
-        System.out.println(result); //Less than 100
-    }
-}
