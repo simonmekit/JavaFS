3fc6b3828620857201d2ab273e0dc20c80a64844
simon mekit <simonmekit@gmail.com>
2023-12-17 22:27:14 -0500

    Read me file update1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
